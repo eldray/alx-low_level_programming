b6b0a6a965eab65939f7a0e6e3e62f0d6a971a35
eldray <emk.appiah@gmail.com>
2022-09-11 15:04:19 -0700

    commit c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 
 {
-	write(1, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(1, "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }

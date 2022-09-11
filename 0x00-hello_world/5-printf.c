98aee88c877bb37d2e5105598917ffd49636c8ec
eldray <emk.appiah@gmail.com>
2022-09-11 14:31:42 -0700

    C Commits

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{ 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n"); 
 	return (0);
 }

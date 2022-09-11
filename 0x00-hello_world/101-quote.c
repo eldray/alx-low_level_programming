a4e994f0333fad2f373cf0f028e9283d89890acf
eldray <emk.appiah@gmail.com>
2022-09-11 14:55:58 -0700

    C Commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-
-#include <stdlib.h>
-
-
+#include <unstdio.h>
 /**
  * main - Entry point
  *
@@ -11,7 +8,7 @@
 int main(void)
 
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
+	write(1, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

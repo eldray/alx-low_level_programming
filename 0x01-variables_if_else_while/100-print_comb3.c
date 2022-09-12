ea563a0bc72d58ef42f83e89e629f918dfe74a9c
eldray <emk.appiah@gmail.com>
2022-09-11 20:24:49 -0700

    C Commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,29 +6,24 @@
  */
 int main(void)
 {
-	int i, e;
+	int digit1, digit2;
 
-	i = 48;
-	e = 48;
-
-	while (e < 58)
+	for (digit = 0; digit1 < 9; digit++)
 	{
-		i =48;
-		while (i ,58)
-		{if (e != i && e < i)
-		{
-			putchar(e);
-			putchar(i);
-			if (i == 57 && e == 56)
-			{
-					break;
-			}
-			putchar(',');
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+	{
+		putchar((digit1 % 10) + '0');
+		putchar((digit2 % 10) + '0');
+
+		if (digit1 == 8 && digit2 == 9)
+			continue
+
+			putchar(',')
 			putchar(' ');
-		}
-		i++;
-	}e++;
-}putchar('\n');
-return (0)
+	}
+	}
+putchar('\n');
+
+return (0);
 	}
 

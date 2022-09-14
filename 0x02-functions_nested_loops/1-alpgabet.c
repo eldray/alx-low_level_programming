c8779b148606c3ff5ff246712b91ebfac7befefe
eldray <emk.appiah@gmail.com>
2022-09-14 12:53:56 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alpgabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet - print the alphabet in lowercase, followed by a new line.
+ *
+ */
+
+void print_alphabet(void)
+
+{
+
+		char letter;
+
+			for (letter = 'a'; letter <= 'z'; letter++)
+
+						_putchar(letter);
+
+				_putchar('\n');
+
+}

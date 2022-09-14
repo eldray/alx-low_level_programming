3ddae0e691b8e857a0b1a69c80f73c2311ef9331
eldray <emk.appiah@gmail.com>
2022-09-14 12:57:43 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ * add - adds two numbers anr produce the answer
+ * @a:first integer value
+ * @b:second integer value
+ * 
+ * Return: the summation.
+ */
+int add(int a, int b)
+{
+		return (a + b);
+}

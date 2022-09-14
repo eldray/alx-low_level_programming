31dd32232f78d910e17bce3e1efb9b84d3557a20
eldray <emk.appiah@gmail.com>
2022-09-13 17:27:44 -0700

    new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -Print out main car by char
+ * deescription: uses the main header file
+ * Return: 0
+ */
+int main(void)
+	{
+		char string[] = "_putchar";
+		int i;
+
+		i = 0
+			while (str[i])
+			{
+				_putchar(str[i]); ++i;
+			}
+		_putchar('\n');
+
+		return (0);
+	}

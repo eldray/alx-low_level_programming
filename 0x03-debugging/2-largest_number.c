dba5014c7660f0d0e70256e45dd4a12a79e07a32
eldray <emk.appiah@gmail.com>
2022-09-15 15:40:36 -0700

    new

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -18,22 +18,20 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a > b && b > c)
-
 	{
 		largest = a;
 	}
 
 	else if (b > a && a > c)
-
 	{
 		largest = b;
 	}
 
 	else
-
 	{
 		largest = c;
 	}
+
 	return (largest);
 
 }

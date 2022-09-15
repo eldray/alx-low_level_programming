2eca2445acfb3a4f0d2bc67b0baeba27bb493c75
eldray <emk.appiah@gmail.com>
2022-09-15 16:38:34 -0700

    new

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ *_isdigit - checks if an int is a char digit
+ *
+ * @c: is the into to check if char digit
+ *
+ * Return: 0 if is not digit and 1 if is a digit
+ */
+
+int _isdigit(int c)
+
+{
+
+	if (c >= '0' && c <= '9')
+
+		return (1);
+
+	return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+
+/**
+ *print_line - prints a line with the length being the input
+ *
+ * @n: the length of the line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+
+	_putchar('_');
+
+	_putchar('\n');
+}

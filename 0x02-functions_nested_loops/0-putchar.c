d2c6e2d98bfdc9906aaf1427443f7994b45ea0f4
eldray <emk.appiah@gmail.com>
2022-09-14 22:11:33 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
+
+
 /**
- * main - print putchar
  *
- * Return: always 0
+ * main - Entry point
+ *
+ *  Description: prints _putchar using putchar prototype
+ *  
+ * Return: Always 0 (Success)
+ *
  */
 
 int main(void)
 
-{	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+	_putchar(str[ch]);
+
 	_putchar('\n');
+
 	return (0);
+
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,14 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include<stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alpahbet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int n)
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
@@ -15,4 +17,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
-#endif
+#endif /*MAIN_H*/

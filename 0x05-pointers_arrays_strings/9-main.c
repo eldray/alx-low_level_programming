0291bc4842bb0188f211e6b57b0120dd3c0afa79
eldray <emk.appiah@gmail.com>
2022-09-20 16:01:24 -0700

    arrays, pointers and strings

---
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *
+ *    * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	  char *str;
+
+	    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	      _puts(str);
+
+	        return (0);
+
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+`#include <stdio.h>
 
 /**
  *  _strcpy - Copy a string

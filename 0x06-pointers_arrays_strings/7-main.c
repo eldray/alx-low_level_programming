96ef070481fda02c1e57d61d545001c6c32de402
eldray <emk.appiah@gmail.com>
2022-09-22 12:11:42 -0700

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	char s[] = "Expect the best and Capitalize on what comes.\n";
+	char *p;
+
+	      p = leet(s);
+
+	      printf("%s", p);
+
+		  printf("%s", s);
+
+		return (0);
+}

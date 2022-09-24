e58b6e083fd87589d4f218199b9d49caaf5df487
eldray <emk.appiah@gmail.com>
2022-09-23 22:21:52 -0700

    new

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
 
-        a[2] = 1024;
+	a[2] = 1024;
 	p = &n;
 
 /*
@@ -19,6 +18,7 @@ int main(void)
  */
 	 *(p + 5) = 98;
  /* ...so that this prints 98\n */
- printf("a[2] = %d\n", a[2]);
+
+	 printf("a[2] = %d\n", a[2]);
 	return (0);
 }
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
- *  rev_string - reverse array
- *  @n: integer params
+ * rev_string - reverse array
+ * @n: integer params
  * Return: 0
  */
 void rev_string(char *n)
-{int i = 0;
+{	int i = 0;
 	int j = 0;
 	char temp;
 
 	while (*(n + i) != '\0')
-	{i++; }
+	{ i++; }
 	i--;
+
 	for (j = 0; j < i; j++, i--)
-	{temp = *(n + j);
+	{	temp = *(n + j);
 		*(n + j) = *(n + i);
-		*(n + i) = temp; } }
+		*(n + i) = temp;
+	} }
 /**
- *  infinite_add - add 2 numbers together
+ *infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
  * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
@@ -25,11 +27,12 @@ void rev_string(char *n)
  * Return: pointer to calling function
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{int overflow = 0, i = 0, j = 0, digits = 0;
+{	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
-
 	while (*(n1 + i) != '\0')
+
 		i++;
+
 	while (*(n2 + j) != '\0')
 		j++;
 	i--;
@@ -42,12 +45,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (i < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + i) - '0';
+			val1 = *(n1 + i)-'0';
 
 		if (j < 0)
 			val2 = 0;
 		else
-			val2 = *(n2 + j) - '0';
+			val2 = *(n2 + j)-'0';
 		temp_tot = val1 + val2 + overflow;
 
 		if (temp_tot >= 10)

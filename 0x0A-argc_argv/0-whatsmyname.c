ad63c81d7a88eddcae3466d666e16f5358122978
eldray <emk.appiah@gmail.com>
2022-09-30 22:30:23 -0700

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 
-
-
 /**
+ *  main - prints the name of the program
  *
- *  * main - prints the name of the program
+ * @argc: number of arguments passed to the function
  *
- *   * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- *    * @argv: argument vector of pointers to strings
- *
- *     *
- *
- *      * Return: always 0
- *
- *       */
+ * Return: always 0
+ */
 
 int main(int argc __attribute__((unused)), char *argv[])
-
 {
-
 	printf("%s\n", argv[0]);
-
 	return (0);
-
 }

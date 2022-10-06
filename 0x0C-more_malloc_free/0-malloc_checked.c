0da2b7a3f5044c1edb391ffae180bbcd1d5f2ec3
eldray <emk.appiah@gmail.com>
2022-10-06 15:26:46 -0700

    More Malloc , Free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include <stdlib>
+
+/*
+ * *malloc-cheked - allocates memory to the checker
+ * @b : buffer size
+ * Return: buffer to print
+ */
+
+void *malloc-checked(unsigned int b)
+{
+	void *p
+
+malloc
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This projects contains files on More Malloc , Free

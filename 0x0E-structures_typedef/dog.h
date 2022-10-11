8f81f2b303cb41eae05623f8449168b2f25848cd
eldray <emk.appiah@gmail.com>
2022-10-11 03:46:21 -0700

    new struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,8 @@
-#ifndef FILE_DOG
-#define FILE_DOG
+#ifndef DOG_H
+#define DOG_H
 
 /**
- *struct dog - structure dog
+ * struct dog - structure dog
  * @name: atribute pointer to char name's dog
  * @age: atribute float age's dog
  * @owner: atribute pointer to char owner's dog
@@ -17,7 +17,7 @@ char *owner;
 
 dog_t;
 /* Prototype for other functions*/
-init _putchar(char c);
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

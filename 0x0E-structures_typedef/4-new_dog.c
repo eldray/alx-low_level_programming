e9d706e0cfd9e4e7d405edb9c2839c65909ecbcc
eldray <emk.appiah@gmail.com>
2022-10-11 22:53:16 -0700

    new structs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,43 +1,81 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
-/**
- * new_dog - new dog
- * @name: name's dog
- * @age: age's dog
- * @owner: owner's dog
- * Return: newdog
+int _strLen(char *str);
+void fillMem(char *str, int strLen, char *dest);
+
+/*
+ * *new_dog - Creates a new dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ * Return: Pointer to the newly created dog (SUCCESS) or
+ * NULL if insufficient memory was available (FAILURE)
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int nlen, olen, i;
-	dog_t *doggy;
+	dog_t *n_dog;
+	int nameLen, ownerLen;
 
-	while (name[nlen++])
-		;
-	while (owner[olen++])
-		;
-	doggey = malloc(sizeof(dog_t));
-	if (doggy == NULL)
-	free(doggy);
+	n_dog = malloc(sizeof(dog_t));
+
+if (n_dog == NULL)
+
+return (NULL);
+
+	nameLen = _strLen(name);
+	n_dog->name = malloc(sizeof(char) * nameLen + 1);
+	if (n_dog->name == NULL)
+	{
+	free(n_dog);
 	return (NULL);
+	}
+	fillMem(name, nameLen, n_dog->name);
+	ownerLen = _strLen(owner);
+	n_dog->owner = malloc(sizeof(char) * ownerLen + 1);
+
+	if (n_dog->owner == NULL)
+	{
+	free(n_dog);
+	free(n_dog->name);
+	return (NULL);
+	}
+	fillMem(owner, ownerLen, n_dog->owner);
+	n_dog->age = age;
+	return (n_dog);
 }
-doggy->name = malloc(nlen * sizeof(doggy->name));
+/*
+ * _strLen - Get length of a string
+ *
+ * @str: A string
+ *
+ * Return: Length of string
+ */
 
-if (doggy == NULL)
+int _strLen(char *str)
+{
+	int i = 0;
 
-return (NULL);
+	while (str[i])
 
-for (k = 0; k <= nlen; k++)
-	doggy->name[k] = name[k];
-	doggy->age = age;
+	i++;
+return (i);
+}
 
-doggy->owner = malloc(olen * sizeof(doggy->owner));
-if (doggy == NULL)
+/*
+ * fillMem - Copy string literal to allocated memory
+ * @str: String literal
+ * @strLen: @str length
+ *
+ * @dest: The allocated memory
+ */
 
-return (NULL);
-	for (k = 0; -k < olen; k++)
-		doggy->owner[k] = owner[k];
-return (doggy);
+void fillMem(char *str, int strLen, char *dest)
+{
+	int i;
+
+	for (i = 0; i < strLen; i++)
+	dest[i] = str[i];
+	dest[i] = '\0';
 }

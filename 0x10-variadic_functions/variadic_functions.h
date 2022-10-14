77b976f2f7c60b12e6ad8268f4daa43eff184c1a
eldray <emk.appiah@gmail.com>
2022-10-14 12:43:34 -0700

    var func

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct print - print type with corresponding print func
+ * @t: print type
+ * @f: print function
+ */
+
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
 int _putchar.c
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);

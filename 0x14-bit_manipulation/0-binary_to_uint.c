29568e320033d997bf3ec7264afa24aeb4f1e12f
eldray <emk.appiah@gmail.com>
2022-10-28 22:56:45 -0700

    bit manipulations

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,22 +2,26 @@
 
 /**
  * binary_to_unit - converts a binary number to an unsigned int
- * @b - pointer to a string that contains a binary number 
+ * @b: pointer to a string that contains a binary number
  * Return: an unsigned int with a decimal
  */
 
 unsigned int binary_to_unit(const char *b)
 {
+	unsigned int base2 = 1;
+	unsigned int base10 = 0;
 	unsigned int exp;
 
 	if (!*b)
-		return 0;
+	{
+		return (0);
+	}
 
-	binary_to_unit((const char*) b + 1);
+	binary_to_unit((const char *) b + 1);
 
-	exp = (*(b +1) == ‘\o’) ? 1 : 2;
+	exp = (*(b + 1) == ‘\o’) ? 1 : 2;
 	base2 *= exp;
 	base10 += base2 * (*b - 48);
 
-	return base10;
+	return (base10);
 }

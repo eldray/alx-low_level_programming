938a07982a9696685940d03546a3f34ef4d65557
eldray <emk.appiah@gmail.com>
2023-03-04 19:00:40 -0800

    makefile corrections

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,20 +3,24 @@
 
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
-    c = 0
-    length = len(grid) - 1
-    width = len(grid[0]) - 1
+    """Returns the perimeter of the island described in grid
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+            The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    for i, r in enumerate(grid):
-        for j, n in enumerate(r):
-            if n == 1:
-                if i == 0 or grid[i - 1][j] != 1:
-                    c += 1
-                if j == 0 or grid[i][j - 1] != 1:
-                    c += 1
-                if j == width or grid[i][j + 1] != 1:
-                    c += 1
-                if i == length or grid[i + 1][j] != 1:
-                    c += 1
-    return c
+    for i in range(height):
+    for j in range(width):
+    if grid[i][j] == 1:
+        size += 1
+    if (j > 0 and grid[i][j - 1] == 1):
+        edges += 1
+    if (i > 0 and grid[i - 1][j] == 1):
+        edges += 1
+        return size * 4 - edges * 2

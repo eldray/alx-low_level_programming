bf26fdf180bc9797b4b69608b4d0e6227e6e49f6
eldray <83376852+eldray@users.noreply.github.com>
2023-12-12 03:51:04 +0000

    dynamic libraries

---
+++ 0x18-dynamic_libraries/maths/maths.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "maths.h"
+/**
+ * a simple function to perform arithmetic operations
+ * (+, -, *, /, %) on two integers
+ * @a: the first integer
+ * @b: the second inteer
+ *
+ * Return: the result of the arithmetic performed
+ */
+
+int main(void)
+{
+int add(int a, int b);
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int div(int a, int b)
+{
+	return (a - b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+return 0;
+}
